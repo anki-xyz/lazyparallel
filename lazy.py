f86e9d9e9cbebfe2bbcc8fa6df7ad9e0b583f8e2
anki-xyz <11031615+anki-xyz@users.noreply.github.com>
2019-10-29 11:20:29 +0100

    Different import and added thread functionality

--- lazy.py
+++ lazy.py
@@ -1,6 +1,6 @@
 import numpy as np
 import multiprocessing as mp
-from multiprocessing import Pool
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 from time import sleep, time
 import sys
 
@@ -55,7 +55,7 @@ class LazyParallel:
     """Creates a process pool using the imap function
     of multiprocessing and reports progress"""
 
-    def __init__(self, func, iterable=None, cores='auto'):
+    def __init__(self, func, iterable=None, cores='auto', use_threads=False, threads=1):
         """Creates a process pool using the imap function
         of multiprocessing and reports progress
         
@@ -72,6 +72,9 @@ class LazyParallel:
         """
 
         self.cores = mp.cpu_count() if cores == 'auto' else cores
+        self.threads = threads
+        self.use_threads = use_threads
+
         self.f = func
         self.f_name = func.__name__
         self.i = iterable
@@ -120,11 +123,21 @@ class LazyParallel:
             raise Exception("no iterable found, please provide one")
 
         # Create all processes with number of cores
-        p = Pool(self.cores)
+        #  or threads with number of threads
+        if self.use_threads:
+            p = ThreadPoolExecutor(self.threads)
+        else:
+            p = ProcessPoolExecutor(self.cores)
+
         num_tasks = len(iterable)
 
         if verbose:
-            print('Running {} in parallel on {:d} cores.'.format(self.f_name, self.cores))
+            if self.use_threads:
+                print('Running {} on {:d} threads.'.format(self.f_name, self.threads))
+
+            else:
+                print('Running {} in parallel on {:d} cores.'.format(self.f_name, self.cores))
+
             print('Number of tasks: {:d}'.format(num_tasks))
 
         rl = []
@@ -132,7 +145,7 @@ class LazyParallel:
         t = np.array([0]*(num_tasks+1))
         t[0] = time()
 
-        for i, r in enumerate(p.imap(func, iterable), 1):
+        for i, r in enumerate(p.map(func, iterable), 1):
             # If one would like to see progress
             if verbose:
                 # Set current time
@@ -160,7 +173,7 @@ class LazyParallel:
 
         return rl
 
-def f(i):
+def f_sleep(i):
     """Function that does nothing than sleeping for 1 second
     
     Parameters
@@ -177,11 +190,48 @@ def f(i):
     sleep(1)
     return i
 
+def f_cpu_heavy(i):
+    """Function that does nothing than sleeping for 1 second
+    
+    Parameters
+    ----------
+    i : int
+        a variable that does nothing
+    
+    Returns
+    -------
+    int
+        the input variable
+    """
+    q = 0
+
+    for k in range(10**7):
+        q += k ** 2
+
+    return i
+
 
 if __name__ == '__main__':
-    # Test if everything works
-    cores = mp.cpu_count()
-    l = LazyParallel(f, range(cores*4))
-    r = l.run(verbose=True)
-    print(r)
-    # [0, 1, 2, 3, ...]
+    # Showcase the difference between threads and processes.
+
+    for f in [f_cpu_heavy, f_sleep]:
+        print("Testing {} function: ".format(f.__name__))
+
+        # Test if everything works
+        print("Using processes...")
+        cores = mp.cpu_count()
+        l = LazyParallel(f, range(16), cores=4)
+        r = l.run(verbose=True)
+        print(r)
+        # [0, 1, 2, 3, ...]
+
+        print()
+
+        print("Using threads...")
+        l = LazyParallel(f, range(16), threads=8, use_threads=True)
+        r = l.run(verbose=True)
+        print(r)
+        # [0, 1, 2, 3, ...]
+
+        print()
+        print()
